49881be349054eaff09740c06a03ad8517ce8705
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-12-02 18:49:00 -0300

    getDetailsByFilters added

--- src/main/java/com/alkemy/icons/controller/IconController.java
+++ src/main/java/com/alkemy/icons/controller/IconController.java
@@ -1,6 +1,7 @@
 package com.alkemy.icons.controller;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alkemy.icons.dto.IconBasicDTO;
@@ -24,44 +26,58 @@ public class IconController {
 
 	@Autowired
 	private IconService iconService;
-	
-	
+
 	@GetMapping("/all")
 	public ResponseEntity<List<IconBasicDTO>> getAll() {
 		List<IconBasicDTO> icons = this.iconService.getAllIcons();
 		return ResponseEntity.ok(icons);
 	}
-	
+
+	/*
+	 * Busqueda por id
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<IconDTO> getDetailsById(@PathVariable Long id) {
 		IconDTO icon = this.iconService.getDetailsById(id);
 		return ResponseEntity.ok(icon);
 	}
-	
+
+	/*
+	 * Busqueda por filtros
+	 */
+	@GetMapping
+	public ResponseEntity<List<IconDTO>> getDetailsByFilters(@RequestParam(required = false) String nombre,
+			@RequestParam(required = false) String fechaCreacion, @RequestParam(required = false) Set<Long> paises,
+			@RequestParam(required = false) String orden) {
+		
+		List<IconDTO> icons = this.iconService.getByFilters(nombre, fechaCreacion, paises, orden);
+		return ResponseEntity.ok(icons);
+	}
+
 	@PostMapping
 	public ResponseEntity<IconDTO> save(@RequestBody IconDTO icon) {
 		IconDTO result = this.iconService.save(icon);
 		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
-	
+
 	@PutMapping("/{id}")
 	public ResponseEntity<IconDTO> update(@PathVariable Long id, @RequestBody IconDTO icon) {
 		IconDTO result = this.iconService.update(id, icon);
 		return ResponseEntity.ok(result);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		this.iconService.delete(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
-	
+
 	@PostMapping("/{id}/pais/{idPais")
 	public ResponseEntity<Void> addPais(@PathVariable Long id, @PathVariable Long idPais) {
 		this.iconService.addPais(id, idPais);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
-	
+
 	@DeleteMapping("/{id}/pais/{idPais")
 	public ResponseEntity<Void> removePais(@PathVariable Long id, @PathVariable Long idPais) {
 		this.iconService.removePais(id, idPais);

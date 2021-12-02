653ca024ba07efa4d97602a098ebef6e4431a8f5
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-12-02 18:51:21 -0300

    method findAll added

--- src/main/java/com/alkemy/icons/repository/IconRepository.java
+++ src/main/java/com/alkemy/icons/repository/IconRepository.java
@@ -1,5 +1,8 @@
 package com.alkemy.icons.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.alkemy.icons.entity.IconEntity;
 @Repository
 public interface IconRepository extends JpaRepository<IconEntity, Long> {
 
+	List<IconEntity> findAll(Specification<IconEntity> specification);
 }
